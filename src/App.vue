e03af469f1dccc2ca0d2c47fda626ca8129fa7f5
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2024-05-18 17:07:20 +0400

    Add checkbox atom component with variants

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { Checkbox } from '@/components/atoms';
+export default {
+  components: {
+    Checkbox,
+  },
+  data() {
+    return {
+      isChecked: false,
+    };
+  },
+};
 </script>
 
 <template>
   <div>
     <h1>Atomic Frontend</h1>
     <button class="button">button</button>
+    <div class="checkbox-container">
+      <div class="checkbox-column">Default: <Checkbox v-model="isChecked" /></div>
+      <div class="checkbox-column">Checked: <Checkbox v-model="isChecked" class="checkbox--checked" /></div>
+      <div class="checkbox-column">Interminate: <Checkbox v-model="isChecked" intermediate /></div>
+      <div class="checkbox-column">Interminate Disable: <Checkbox v-model="isChecked" intermediate :disabled="true" /></div>
+      <div class="checkbox-column">Disable: <Checkbox v-model="isChecked" :disabled="true" /></div>
+    </div>
   </div>
 
 </template>
 
 <style scoped>
-
+.checkbox-container {
+    margin-top: 30px;
+    border: 1px dotted;
+    max-width: max-content;
+    display: flex;
+    flex-wrap: wrap;
+    column-gap: 30px;
+    padding: 10px 30px;
+}
+.checkbox-column {
+    display: flex;
+    align-items: center;
+    column-gap: 10px;
+}
 </style>
---
+++ src/components/atoms/Checkbox.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    :class="[
+      'checkbox',
+      { 'checkbox--checked': isChecked },
+      { 'checkbox--intermediate': isIntermediate },
+      { 'checkbox--disabled': isDisabled }
+    ]"
+    @click="toggleCheck"
+  >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Checkbox',
+  props: {
+    value: Boolean,
+    intermediate: Boolean,
+    disabled: Boolean,
+  },
+  data() {
+    return {
+      isChecked: this.value,
+      isIntermediate: this.intermediate,
+      isDisabled: this.disabled
+    };
+  },
+  methods: {
+    toggleCheck() {
+      if(this.intermediate) return
+      if (!this.isDisabled) {
+        this.isChecked = !this.isChecked;
+        this.$emit('input', this.isChecked);
+      }
+    },
+  },
+  watch: {
+    value(newVal) {
+      this.isChecked = newVal;
+    },
+    intermediate(newVal) {
+      this.isIntermediate = newVal;
+    },
+    disabled(newVal) {
+      this.isDisabled = newVal;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/index.scss';
+</style>
